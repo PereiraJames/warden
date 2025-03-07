ff3c1c51219daf8613253327f97bd7b8b5ff3a24
PereiraJames <jamespereira_5@hotmail.com>
2025-03-07 19:13:00 +0800

    warden_init

---
+++ warden.py
@@ -0,0 +1,28 @@
+import nmap
+
+def warden_scan(network_range):
+    nm = nmap.PortScanner()
+
+    nm.scan(hosts=network_range, arguments="-sn")
+
+    devices = []
+    for host in nm.all_hosts():
+        device_info = {
+            'IP': host,
+            'Hostnames': nm[host].hostname(),
+            'Status': nm[host].state(),
+            'Protocols': nm[host].all_protocols(),
+        }
+        devices.append(device_info)
+    
+    return devices
+
+devices = warden_scan(network_range)
+
+# Print out the collected data
+for device in devices:
+    print(f"IP Address: {device['IP']}")
+    print(f"Hostname: {device['Hostnames']}")
+    print(f"Status: {device['Status']}")
+    print(f"Protocols: {device['Protocols']}")
+    print('-' * 40)
